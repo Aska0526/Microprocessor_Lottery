b3f24685b7282cb5e2ddf5f6bf794af164337241
cf619 <cf619@skbl-422-008.ph.ic.ac.uk>
2021-11-30 09:29:35 +0000

    the timer module is functioning (supposingly)

---
+++ random.s
@@ -0,0 +1,23 @@
+#include <xc.inc>
+    
+global	timer
+psect	udata_acs   
+delay_count:	ds 1	
+    
+psect	ran_code,class=CODE
+
+
+timer:
+	movlw	01001000B; set the timer in 8-bit mode, no pre-scaling
+	movwf	T0CON, A
+	movlw	0x00; set initial value for timer0low
+	movwf	TMR0L, A
+	bcf	INTCON, 2; clear off timer0if bit
+	bsf	T0CON, 7; start timer
+	xyz:
+	    btfss   INTCON, 2; see when the timer reaches the roll-over point
+	    bra	    xyz
+	bcf	T0CON, 7; close the timer
+	movf    TMR0L, W, A; display result
+	return
+
