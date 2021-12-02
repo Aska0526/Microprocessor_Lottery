e778fc26c26ca10f03e635bf239c0e7493dda07a
cf619 <cf619@skbl-422-008.ph.ic.ac.uk>
2021-12-02 11:56:53 +0000

    keypad module

--- main.s
+++ main.s
@@ -1,42 +1,75 @@
     #include <xc.inc>
-    ;#define TMR1_IF PIR1, TMR1IF
-    ;#define TMR1_ON T1CON, TMR1ON
+
+psect	udata_bank4 ; reserve data anywhere in RAM (here at 0x400)
+myArray:    ds 0x80 ; reserve 128 bytes for message data
+    
+psect	data    
+	; ******* myTable, data in programme memory, and its length *****    
+myTable:
+	db	'Y','o','u',' ','w','i','n',':',0x0a
+					; message, plus carriage return
+	myTable_l   EQU	8	; length of data
+	align	2    
     
 psect	udata_acs   ; named variables in access ram
-pull:		ds 1   ; draw a lottery
-temp_1:	ds 1
-temp_2:	ds 1
-temp_3:	ds 1
+pull:	    ds 1   ; draw a lottery
+check:      ds 1   ; check balance
+ifIR:       ds 1   ; reset
+temp_1:	    ds 1
+temp_2:	    ds 1
+temp_3:	    ds 1
+counter:    ds 1    ; reserve one byte for a counter variable
+delay_count:ds 1    ; reserve one byte for counter in the delay routine
 
 extrn   Keypad_read_column
 extrn   timer_setup, timer_read
+extrn   LCD_Setup, LCD_Write_Message, LCD_Send_Byte_I
     
 psect	code, abs
 setup:
 	org	0x0
 	
+	bcf	CFGS	; point to Flash program memory  
+	bsf	EEPGD 	; access Flash program memory
+	call	LCD_Setup	; setup LCD
 	call	timer_setup
 	
 	movlw   0x0
-	movwf   TRISB, A
+	movwf   TRISC, A
+	movwf   TRISD, A
 	
 	movlw   00001110B ;give value to pull
 	movwf   pull
+	movlw   00001101B ;give value to check
+	movwf   check
+	movlw   00001011B ;give value to ifIR
+	movwf   ifIR
 	
 	goto    main
+
+start: 	lfsr	0, myArray	; Load FSR0 with address in RAM	
+	movlw	low highword(myTable)	; address of data in PM
+	movwf	TBLPTRU, A		; load upper bits to TBLPTRU
+	movlw	high(myTable)	; address of data in PM
+	movwf	TBLPTRH, A		; load high byte to TBLPTRH
+	movlw	low(myTable)	; address of data in PM
+	movwf	TBLPTRL, A		; load low byte to TBLPTRL
+	movlw	myTable_l	; bytes to read
+	movwf 	counter, A		; our counter register
+
+loop: 	tblrd*+			; one byte from PM to TABLAT, increment TBLPRT
+	movff	TABLAT, POSTINC0; move data from TABLAT to (FSR0), inc FSR0	
+	decfsz	counter, A		; count down to zero
+	bra	loop		; keep going until finished
+		
+	movlw	myTable_l	; output message to LCD
+	addlw	0xff		; don't send the final carriage return to LCD
+	lfsr	2, myArray
+	call	LCD_Write_Message
 	
-main:
-	call    Keypad_read_column
-	movf	PORTE, W
-	cpfseq	pull ;compare if it is the 1st column, if it is then skip next line
-	bra     main
-	call    timer_read
-	movwf   PORTB
-	call    delay_1s
-	bra     main
-	;other condition which I haven't thought of
-	;bra	
-	
+	;goto	$		; goto current line in code
+	return
+
 delay_1s:
 	banksel	temp_1
 	movlw	0xFF
@@ -45,12 +78,49 @@ delay_1s:
 	movlw	0x05
 	
 	movwf	temp_3
-	decfsz	temp_1, f
+	decfsz	temp_1
 	goto	$-1
-	decfsz	temp_2, f
+	decfsz	temp_2
 	goto	$-3
-	decfsz	temp_3, f
+	decfsz	temp_3
 	goto	$-5
+	return
+	
+main:
+	call    Keypad_read_column
+	call    drawing
+	call    balance_check
+	call    IR
+	bra     main
+	;other condition which I haven't thought of
+	;bra	
+	
+drawing:
+	movf	PORTE, W
+	cpfseq	pull, A ;compare if it is the 1st column, if it is then skip next line
+	return
+	call    timer_read
+	movwf   PORTC
+	movlw	00000001B	; display clear
+	call	LCD_Send_Byte_I
+	call    start
+	call    delay_1s
+	return
+
+balance_check:
+	movf	PORTE, W
+	cpfseq	check, A
+	return
+	movlw   0xff
+	movwf   PORTD
+	return
+	
+IR:
+	movf	PORTE, W
+	cpfseq	ifIR, A
+	return
+	movlw   0x01
+	movwf   PORTD
 	return
 	
 	end	main
